4742f3df91e07b574671c11e185aa8b84779a342
Calgary Michael <cseth.michael@gmail.com>
2017-02-27 23:11:14 -0600

    Database initilization

---
+++ PPOK_System/PPOK_System/Controllers/DatabaseController.cs
@@ -0,0 +1,18 @@
+﻿using PPOK_System.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PPOK_System.Controllers {
+	public class DatabaseController : Controller {
+		Database db = new Database();
+
+		// GET: Database
+		public ActionResult Index() {
+			db.initDatabase();
+			return View();
+		}
+	}
+}
---
+++ PPOK_System/PPOK_System/Service/Database.cs
@@ -0,0 +1,49 @@
+﻿using Dapper;
+using PPOK_System.Service.SQL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace PPOK_System.Service {
+	public class Database {
+		private string connection = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+		private string master = ConfigurationManager.ConnectionStrings["MasterConnection"].ConnectionString;
+
+		#region Database Creation
+
+		// Initialize the Database
+		public void initDatabase() {
+			CreateDatabase();
+			CreateTables();
+			//InsertData();
+		}
+
+		// Drop & Create Database
+		private void CreateDatabase() {
+			using (IDbConnection db = new SqlConnection(master)) {
+				db.Execute(Scripts.CreateDatabaseSql);
+			}
+		}
+
+
+		// Create Tables to fill the Database
+		private void CreateTables() {
+			using (IDbConnection db = new SqlConnection(connection)) {
+				db.Execute(Scripts.CreateTablesSql);
+			}
+		}
+
+
+		// Fill tables with dummy data
+		private void InsertData() {
+			using (IDbConnection db = new SqlConnection(connection)) {
+				db.Execute(Scripts.InsertDummyDataSql);
+			}
+		}
+
+		#endregion
+	}
+}
