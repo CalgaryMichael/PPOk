ce5bbdd2092dbc4993f7071956b03bfb32af312f
Weston Buck <west_buck@hotmail.com>
2017-03-18 23:25:17 -0500

    Pharmacy controller, editcustomer, and recall made. Needs some more styling but eh.

---
+++ PPOK_System/PPOK_System/Controllers/PharmacyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PPOK_System.Controllers
+{
+    public class PharmacyController : Controller
+    {
+        // GET: Pharmacy
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EditCustomer()
+        {
+            return View();
+        }
+
+        public ActionResult Recall()
+        {
+            return View();
+        }
+    }
+}
