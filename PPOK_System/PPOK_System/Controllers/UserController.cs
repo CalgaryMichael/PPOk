7a6fd1ad85b0a5e3d5bc4c902235fa283c0871e3
Calgary Michael <cseth.michael@gmail.com>
2017-03-22 14:55:16 -0500

    Removed Sending Message on Page Load

--- PPOK_System/PPOK_System/Controllers/UserController.cs
+++ PPOK_System/PPOK_System/Controllers/UserController.cs
@@ -12,16 +12,11 @@ namespace PPOK_System.Controllers {
 
 		public ActionResult Index()
         {
-			Twilio.TwilioTest.sendmsg("Your meds are ready to pick up.");
-
-
 			return View();
 		}
 
         public ActionResult CatchAll()
         {
-
-
             return View();
         }
     }
