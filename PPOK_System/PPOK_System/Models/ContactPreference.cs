89b631ec1b16fdd0f11b07ebd0ad132d942a8469
colby <colbydial@gmail.com>
2017-03-29 00:06:13 -0500

    Added models to views, added different typse of preferences to contactPreferences model, prepped user contact preferences page to implement db and views together

--- PPOK_System/PPOK_System/Models/ContactPreference.cs
+++ PPOK_System/PPOK_System/Models/ContactPreference.cs
@@ -3,8 +3,10 @@
 		public int preference_id { get; set; }
 		public int person_id { get; set; }
 		public string contact_type { get; set; }
-		public string preference { get; set; }
+		public string refillPreference { get; set; }
+        public string recallPreference { get; set; }
 
-		public Person person { get; set; }
+
+        public Person person { get; set; }
 	}
 }
