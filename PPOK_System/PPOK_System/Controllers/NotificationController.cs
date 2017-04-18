dd86b61035222b41080be93b96c50ddefe0fdbd4
Wsloan0001 <westonvidaurri@gmail.com>
2017-04-17 20:25:58 -0500

    Reply from patient working
    Added simple function to get replys from patients if they want a refill
    or not

---
+++ PPOK_System/PPOK_System/Controllers/NotificationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Mvc;
+
+namespace PPOK_System.Controllers {
+    public class NotificationController : Controller {
+        TwilioManager.TwManager TW = new TwilioManager.TwManager();
+        public void ScheduleNotification() {
+            TW.ScheduleSend();
+        }
+        public void SchedulerStart()
+        {
+            TW.StartHangfire();
+        }
+
+        [HttpPost]
+        public ActionResult replyTest(SmsRequest request)
+        {
+           
+            if (request.Body == "1")
+            {
+                TW.SendResponse(request.From, "we will start your refill");
+            }
+            else if (request.Body == "2")
+            {
+                TW.SendResponse(request.From, "we will not refill your medication");
+            }
+            return null;
+        }
+    }
+}
--- PPOK_System/PPOK_System/Service/Twilio/TwilioManager.cs
+++ PPOK_System/PPOK_System/Service/Twilio/TwilioManager.cs
@@ -7,6 +7,7 @@ using Twilio.Clients;
 using PPOK_System.Domain.Service;
 using PPOK_System.Domain.Models;
 using PPOK_System.Models;
+using Hangfire;
 
 namespace PPOK_System.TwilioManager
 {
@@ -25,6 +26,22 @@ namespace PPOK_System.TwilioManager
                 from: new PhoneNumber("14054000298"),
                 to: new PhoneNumber("14059191824"),
                 url: new Uri("~/Twilio/TwillioXML/PickUp.xml"));
+        }
+
+        public void SendResponse(string phonenum, string msg)
+        {
+            const string accountSid = "ACc4455ec9d784ae580638ecac36ad7fea";
+            const string authToken = "2a6f621c0a94b7e60c906e06e15966fb";
+
+            // Initialize the Twilio client
+            TwilioClient.Init(accountSid, authToken);
+            
+            MessageResource.Create(
+                from: new PhoneNumber("405-400-0298"),
+                to: new PhoneNumber(phonenum),
+
+                body: $"{msg}");
+            
         }
         public void SendNotifications(List<Schedule> scheduleList)
         {
